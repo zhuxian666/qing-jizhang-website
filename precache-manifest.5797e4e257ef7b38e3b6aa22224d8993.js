b14b4ee2c7b5fb341f39fa1bf6d54a44009befdc
猪鲜 <12466331+fresh-pig@user.noreply.gitee.com>
2023-04-20 10:36:54 +0800

    commit message

---
+++ precache-manifest.5797e4e257ef7b38e3b6aa22224d8993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188050b3220a76a8c65a",
+    "url": "/qing-jizhang-website/css/app.4df66372.css"
+  },
+  {
+    "revision": "cf70f41c52de2ee0113d9d4ffdabc41f",
+    "url": "/qing-jizhang-website/index.html"
+  },
+  {
+    "revision": "188050b3220a76a8c65a",
+    "url": "/qing-jizhang-website/js/app.8898d28d.js"
+  },
+  {
+    "revision": "4831b840f3f8fed1598a",
+    "url": "/qing-jizhang-website/js/chunk-vendors.242767fd.js"
+  },
+  {
+    "revision": "81dafd0f177ac49242f8199606b035d0",
+    "url": "/qing-jizhang-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qing-jizhang-website/robots.txt"
+  }
+]);
